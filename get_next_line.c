cb4a02c8c370120ecb9732b04014338db084e5d9
Dunn Orphan <dorphan@mi-a4.21-school.ru>
2019-10-11 16:43:13 +0300

    Finally it's complitelly working version of GNL. This GNL function uses lists to work whith multipile file deskriptor

--- get_next_line.c
+++ get_next_line.c
@@ -11,52 +11,96 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "Libft/libft.h"
 
-static void		ftail(char *buf, char *tail)
+static int				ft_gnllist(int fd, t_gnllist **head, t_gnllist **list)
 {
-	char *tmp;
-
-	ft_bzero(tail, BUFF_SIZE + 1);
-	tmp = ft_strchr(buf, '\n');
-	ft_strcpy(tail, tmp);
-	*tmp = '\0';
-}
-
-static void		join(char **line, char *buf)
-{
-	char *copy;
-
-	copy = *line;
-	*line = ft_strjoin(copy, buf);
-	free(copy);
-}
-
-int				get_next_line(const int fd, char **line)
-{
-	char			buf[BUFF_SIZE + 1];
-	int				ret;
-	static char		tail[BUFF_SIZE + 1];
-
-	if (fd < 0)
-		return (-1);
-	if (!*line)
-		*line = malloc(sizeof(*line));
+	if (!*head)
+	{
+		if (!(*head = malloc(sizeof(t_gnllist))))
+			return (0);
+		(*head)->file_d = fd;
+		(*head)->next = NULL;
+		(*head)->tail = NULL;
+	}
+	*list = *head;
+	while ((*list)->file_d != fd && (*list)->next)
+		(*list) = (*list)->next;
+	if ((*list)->file_d == fd)
+		return (1);
 	else
 	{
-		free(*line);
-		*line = ft_strdup(tail);
+		if (!((*list)->next = malloc(sizeof(t_gnllist))))
+			return (0);
+		(*list)->next->file_d = fd;
+		(*list)->next->next = NULL;
+		(*list)->next->tail = NULL;
+		*list = (*list)->next;
+		return (1);
 	}
-	while ((ret = read(fd, buf, BUFF_SIZE)))
+}
+
+static void				ft_join(t_gnllist **list)
+{
+	(*list)->buf[(*list)->ret] = '\0';
+	if (!(*list)->tail)
+		(*list)->tail = ft_strdup((*list)->buf);
+	else
 	{
-		buf[ret] = '\0';
-		if (ft_strchr(buf, '\n'))
-		{
-			ftail(buf, tail);
-			join(line, buf);
-			return (1);
-		}
-		join(line, buf);
+		(*list)->copy = (*list)->tail;
+		(*list)->tail = ft_strjoin((*list)->copy, (*list)->buf);
+		free((*list)->copy);
 	}
-	return (0);
+}
+
+static void				ft_separator(t_gnllist **list, char **line, int *i)
+{
+	char				*tmp;
+	char				*ptr;
+
+	if (!(*list)->tail)
+		(*list)->tail = ft_strdup("\0");
+	tmp = ft_strchr((*list)->tail, '\n');
+	if (!tmp)
+	{
+		*line = ft_strdup((*list)->tail);
+		free((*list)->tail);
+		(*list)->tail = NULL;
+	}
+	else
+	{
+		*i = 1;
+		*line = ft_strsub((*list)->tail, 0, tmp - (*list)->tail + 1);
+		ptr = *line;
+		while (*ptr != '\n' && *ptr != '\0')
+			ptr++;
+		*ptr = '\0';
+		(*list)->copy = ft_strdup(tmp + 1);
+		free((*list)->tail);
+		(*list)->tail = (*list)->copy;
+	}
+}
+
+int						get_next_line(const int fd, char **line)
+{
+	static t_gnllist	*head;
+	t_gnllist			*list;
+	char				*tmp;
+	int					very_important_i;
+
+	if (fd < 0 || !line || BUFF_SIZE <= 0 || !(ft_gnllist(fd, &head, &list)))
+		return (-1);
+	tmp = NULL;
+	very_important_i = 0;
+	while ((list->ret = read(fd, list->buf, BUFF_SIZE)))
+	{
+		if (list->ret < 0)
+			return (-1);
+		ft_join(&list);
+		if ((tmp = ft_strchr(list->buf, '\n')))
+			break ;
+	}
+	ft_separator(&list, line, &very_important_i);
+	if (list->ret == 0 && ft_strlen(*line) == 0 && very_important_i == 0)
+		return (0);
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -10,17 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _GET_NEXT_LINE_H_
-# define _GET_NEXT_LINE_H_
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 10
+# define BUFF_SIZE 100
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
 # include <sys/stat.h>
 # include <sys/types.h>
+# include "libft/libft.h"
 
-int		get_next_line(const int fd, char **line);
+typedef struct			s_gnllist
+{
+	int					file_d;
+	int					ret;
+	char				buf[BUFF_SIZE + 1];
+	char				*tail;
+	char				*copy;
+	struct s_gnllist	*next;
+}						t_gnllist;
+
+int						get_next_line(const int fd, char **line);
 
 #endif
